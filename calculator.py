dea287b26c7a3eaf51256c82a5c52c97769b88ac
Your Name <you@example.com>
2020-01-08 15:39:19 -0800

    added the quit and add functionality.

--- calculator.py
+++ calculator.py
@@ -8,3 +8,15 @@ from arithmetic import *
 
 
 # Your code goes here
+while True:
+    command = input("> ")
+    command = command.split(" ")
+    if command[0] == "q":
+        exit()
+    else:
+        if command[0] == "+":
+            print(add(int(command[1]), int(command[2])))
+
+# def find_function(first):
+#     if first == "+"
+#         return 
